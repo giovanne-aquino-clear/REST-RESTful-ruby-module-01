017e60995ff4f18d4109f0dc703445ad74ae846b
Giovanne <giovanne.aquno.clear@gmail.com>
2023-10-30 12:06:50 -0300

    add migration and scaffold for contact type

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :kinds
   resources :contacts #define todas as rotas automaticamente através de "resources "
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
