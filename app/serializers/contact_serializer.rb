a9651ac501fe98592cd923ae0b6d0eab8343df81
Giovanne <giovanne.aquno.clear@gmail.com>
2023-10-31 12:45:58 -0300

    use AMS and Links (HATEOAS)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
+
+  link(:self) { contact_url(object.id) }
+  link(:kind) { kind_url(object.kind.id) }
+
+  # def author
+  #   "Jackson Pires"
+  # end
 
   meta do
     { author: "Jackson Pires" }
@@ -12,6 +22,7 @@ class ContactSerializer < ActiveModel::Serializer
   def attributes(*args)
     h = super(*args)
     # pt-BR ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
 end
